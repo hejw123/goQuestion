48f71426b307ee3069b660f0275206adfe58f07e
hejw <13552656607@163.com>
2020-11-20 16:40:32 +0800

    通过协程来处理http请求

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 
 func main() {
 	mysql.Setup()
-	http.HandleFunc("/createQuestion" , service.Create )
-	http.HandleFunc("/updateQuestion" , service.Update )
-	http.HandleFunc("/listQuestion" , service.List )
-	http.HandleFunc("/" , service.Show)
-
+	go func() {
+		http.HandleFunc("/createQuestion" , service.Create )
+		http.HandleFunc("/updateQuestion" , service.Update )
+		http.HandleFunc("/listQuestion" , service.List )
+		http.HandleFunc("/" , service.Show)
+	}()
 	log.Fatal(http.ListenAndServe("127.0.0.1:8889" , nil))
 }
